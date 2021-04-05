32da0ffecf8e4964209a40eaf1c8500dbdda842f
bhingkay <79082266+bhingkay@users.noreply.github.com>
2021-04-05 13:32:53 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import "./App.css";
+import Button from "react-bootstrap/Button";
 
 export default function App() {
   return (
     <div className="App">
-      <h2>Weather App</h2>
-      <footer>
-        <p>
-          Open sourced code by: Bhing Rudenas on{" "}
-          <a
-            href="https://github.com/bhingkay/myrreactweatherapp"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        <h2>Weather App</h2>
+        <Button variant="primary">Primary</Button>
+        <footer>
+          <p>
+            Open sourced code by: Bhing Rudenas on{" "}
+            <a
+              href="https://github.com/bhingkay/myrreactweatherapp"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
