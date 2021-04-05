7733d43462f989a740b86494d19072782fb0f567
bhingkay <79082266+bhingkay@users.noreply.github.com>
2021-04-05 12:44:08 +0300

    added github url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ export default function App() {
   return (
     <div className="App">
       <h2>Weather App</h2>
+      <footer>
+        <p>
+          Open sourced code by: Bhing Rudenas on{" "}
+          <a
+            href="https://github.com/bhingkay/myrreactweatherapp"
+            target="_blank"
+          >
+            Github
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
