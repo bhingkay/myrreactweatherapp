0749a45fffbccf7eef611f8b6c91133a4e7c8250
bhingkay <79082266+bhingkay@users.noreply.github.com>
2021-04-10 15:16:47 +0300

    updated css

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="goldenrod"
+      color="rgba(247, 236, 95, 0.925"
       size={70}
       animate={true}
     />
